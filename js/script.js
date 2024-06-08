7016278e454f2ffe6bedfa30cf6e0429ed2f7802
sjastrow <skjastrow@gmail.com>
2024-06-08 16:05:51 -0400

    onclick function to save text value of search and store in array.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ var date = moment().format('dddd, MMMM Do YYYY');
 var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 
 var cityHist = [];
+
+
+//Will save the text value of the search and save it to an array and storage
+$('.search').on("click", function (event) {
+	event.preventDefault();
+	city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
+	if (city === "") {
+		return;
+	};
+	cityHist.push(city);
+
+	localStorage.setItem('city', JSON.stringify(cityHist));
+	fiveForecastEl.empty();
+	getHistory();
+	getWeatherToday();
+});

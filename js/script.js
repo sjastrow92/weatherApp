84048cbca95d2b61c7259d0235639ed726908e99
sjastrow <skjastrow@gmail.com>
2024-06-08 16:05:07 -0400

    started script file by adding varibales.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// tailwinds file
-content: ["./src/**/*.{html,js}"],
+var key = '64f2ee2a8261daa4d9f780f5b365f275';
+var city = "Denver"
 
+//Grabs the current time and date
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 
-// search bar
-import { Ripple, initMDB } from "mdb-ui-kit";
-
-initMDB({ Ripple });
+var cityHist = [];
